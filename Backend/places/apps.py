43f903da22075b35c12f824f4518367dc77636cc
kwoo618 <kangwoo111@naver.com>
2025-10-27 16:17:20 +0900

    병합

--- Backend/places/apps.py
+++ Backend/places/apps.py
@@ -1,10 +1,4 @@
 #앱 구성 정보
-<<<<<<< HEAD
-=======
-# 앱 이름과 설정 정의
-=======
->>>>>>> f0d9ca87a04010bdc1ed6ce86a412fa2f9ef480c
->>>>>>> ac8b761b633b974b8046fcf910b9027598498110
 from django.apps import AppConfig
 
 
