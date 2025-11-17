c931b2f74fa31e8b7ade356b30a41eef64b78658
io8272 <https://github.com/io8272>
2025-11-17 12:44:01 +0900

    certifi==2024.8.30 pytz==2024.1 tzdata==2024.1 3개 버전 현존하지 않는 버전으로 설정돼 있어서 현존하는 버전으로 수정 run.bat에서 redis 는 백그라운드에 숨기고 start하도록 변경

--- Backend/run.bat
+++ Backend/run.bat
@@ -1,2 +1,2 @@
-start "Redis Server" cmd /k "cd /d C:\Program Files\Redis && redis-server.exe redis.windows.conf"
+start /min "" cmd /c "cd /d C:\Program Files\Redis && redis-server.exe redis.windows.conf"
 python manage.py runserver
